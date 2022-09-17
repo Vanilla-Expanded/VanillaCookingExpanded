9c082b695ec08969cb9329139210ca6e42eb4f8a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-09-17 16:01:59 +0200

    Initial commit

---
+++ 1.3/Source/VanillaCookingExpanded/VanillaCookingExpanded/Utils/PawnUtility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace VanillaCookingExpanded
+{
+    public static class PawnUtility
+    {
+        public static bool PawnNotHaveActiveCondimentAsFood(Thing t, bool desperate, Pawn pawn)
+        {
+            if (desperate)
+                return true;
+
+            List<ThoughtDef> hasThoughts =
+                pawn?.needs?.mood?.thoughts?.memories?.Memories
+                    .Where(m => ThoughtDefs.AllThoughts.Contains(m.def))
+                    .Select(m => m.def)
+                    .ToList();
+
+            if (!hasThoughts?.Any() ?? true)
+                return true;
+
+            CompIngredients compIngredients = t.TryGetComp<CompIngredients>();
+            if (compIngredients == null)
+                return true;
+
+            bool result = false;
+            try
+            {
+                result = !compIngredients.ingredients
+                    .Any(def => def != null && def.ingestible!=null && hasThoughts.Contains(def.ingestible.specialThoughtAsIngredient));
+            }
+            catch (Exception e)
+            {
+                Log.Warning($"Something wonky happens to your meal {t}.\n{e}");
+            }
+
+            return result;
+        }
+
+        public static bool IsDesperateForFood(this Pawn pawn)
+        {
+            return pawn.needs.food.CurCategory >= HungerCategory.Hungry;
+        }
+    }
+}
